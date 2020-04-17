7fbfe2d68f4d096124fcc099bcd534c55dcd141c
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-04-16 22:52:26 -0300

    Configurado Django Debug Toolbar
    close #31

--- myapi/settings.py
+++ myapi/settings.py
@@ -74,6 +74,14 @@ TEMPLATES = [
     },
 ]
 
+# Configuração Django Debug Tollbar
+
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
 WSGI_APPLICATION = 'myapi.wsgi.application'
 
 # Database
